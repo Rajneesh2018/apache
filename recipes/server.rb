79b05285499dfb188940b3b10a2d71bf6792e9d6
Rajneesh <rajneesh@mcmcg.com>
2018-10-23 17:16:55 +0000

    used template

--- recipes/server.rb
+++ recipes/server.rb
@@ -9,11 +9,8 @@ package 'httpd' do
 	action :install
 end
 
-file '/var/www/html/index.html' do
-	content "<h1>Hello, world!!</h1>
-		<h3>HOSTNAME: #{node['hostname']}</h3>
-		<h3>CPU: #{node['cpu']['0']['mhz']}</h3>
-		<h3>MEMORY: #{node['memory']['total']}</h3>"
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
 end
 
 service 'httpd' do
